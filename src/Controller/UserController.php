78637e91616e61db720810c31d3a8a6c17d08648
tejiogni <mtejiogni@yahoo.fr>
2025-09-06 17:19:28 +0100

    CRUD sur la table User

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,15 +3,92 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\UserRepository;
 
-final class UserController extends AbstractController
-{
+
+final class UserController extends AbstractController {
+
     #[Route('/user', name: 'user.index')]
-    public function index(): Response {
+    public function index(Request $request, UserRepository $repo): Response {
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'users' => $repo->findAll()
         ]);
+    }
+
+
+    #[Route('/user/edit', name: 'user.edit')]
+    public function edit(): Response {
+        return $this->render('user/edit.html.twig');
+    }
+
+
+    #[Route('/user/register', name: 'user.register', methods: ['GET', 'POST'])]
+    public function register(Request $request, EntityManagerInterface $em): Response {
+        // Recuperation de la session dans la requete
+        $session = $request->getSession();
+
+        //Initialisation de la variable pour la gestion des erreurs
+        $errors= null;
+
+        if($request->isMethod('POST')) {
+            $nom= $request->request->get('nom');
+            $prenom= $request->request->get('prenom');
+            $telephone= $request->request->get('telephone');
+            $email= $request->request->get('email');
+
+            if(empty($nom) || empty($prenom) || empty($telephone) || empty($email)) {
+                $errors= [
+                    "type" => "warning",
+                    "message" => "L'un des champs n'est pas renseigné"
+                ];
+
+                // Stockage dans la session
+                $session->set('errors', $errors);
+                //dd($request->getSession()->get('errors'));
+
+                return $this->render('user/edit.html.twig', [
+                    'params' => $request->request->all()
+                ]);
+            }
+            else {
+                $user = new User();
+                $user->setNom($nom);
+                $user->setPrenom($prenom);
+                $user->setTelephone($telephone);
+                $user->setEmail($email);
+                $user->setCreatedAt();
+                $user->setUpdatedAt();
+
+                $em->persist($user);
+                $em->flush();
+
+                $errors= [
+                    "type" => "success",
+                    "message" => "Utilisateur ajouté avec succès"
+                ];
+                $session->set('errors', $errors);
+
+                return $this->redirectToRoute('user.index');
+            }
+
+            
+        }
+        else {
+            $errors= [
+                "type" => "danger",
+                "message" => "La méthode d'envoie des données est invalide"
+            ];
+            $session->set('errors', $errors);
+
+            return $this->render('user/edit.html.twig');
+        }
+
     }
 }
---
+++ src/Entity/User.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use DateTimeImmutable;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $telephone = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(string $telephone): static
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(): static
+    {
+        $this->createdAt = new DateTimeImmutable();
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(): static
+    {
+        $this->updatedAt = new DateTimeImmutable();
+
+        return $this;
+    }
+}
